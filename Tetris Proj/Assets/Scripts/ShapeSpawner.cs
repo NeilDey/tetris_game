e81bbdb81563aac269ce67176d92321cd7b998fb
Neil Dey <neilrdey@gmail.com>
2017-10-10 21:32:51 +0530

    Initial Commit

---
+++ Tetris Proj/Assets/Scripts/ShapeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeSpawner : MonoBehaviour {
+
+    public GameObject[] shapes;
+
+    public void SpawnShape() {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
