a926a26881cb75aa8b50a02861ab4090bd1cf2ff
Neil Dey <neilrdey@gmail.com>
2017-10-10 22:34:06 +0530

    Added ShapeSpawner and Shape scripts.
    Shape script can only do movements like rotate, left, right and down. No collision detection yet.

---
+++ Tetris Proj/Assets/Scripts/Shape.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("a"))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+
+            Debug.Log(transform.position);
+
+            
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+            Debug.Log(transform.position);
+            
+        }
+
+        if (Input.GetKeyDown("s") )
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            Debug.Log(transform.position);
+
+           
+            
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            transform.Rotate(0, 0, 90);
+
+            Debug.Log(transform.position);
+
+            
+        }
+
+    }
+
+   
+}
--- Tetris Proj/Assets/Scripts/ShapeSpawner.cs
+++ Tetris Proj/Assets/Scripts/ShapeSpawner.cs
@@ -7,12 +7,15 @@ public class ShapeSpawner : MonoBehaviour {
     public GameObject[] shapes;
 
     public void SpawnShape() {
-
+        int shapeIndex = Random.Range(0, 6);
+        Instantiate(shapes[shapeIndex],
+        transform.position,
+        Quaternion.identity);
     }
 
 	// Use this for initialization
 	void Start () {
-		
+        SpawnShape();
 	}
 	
 	// Update is called once per frame
